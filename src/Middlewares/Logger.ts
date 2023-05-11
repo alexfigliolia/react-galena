8e7c0abf21b9a930b54a67cabb32b1412e6089c0
Alex Figliolia <alexfigliolia@gmail.com>
2023-05-10 21:04:11 -0700

    Color logging

--- src/Middlewares/Logger.ts
+++ src/Middlewares/Logger.ts
@@ -1,17 +1,32 @@
+import type { State } from "Galena/State";
 import { Middleware } from "Middleware/Middleware";
-import type { State } from "galena/State";
 
 export class Logger extends Middleware {
-  private previousState: State | null = null;
+  private previousState: Record<string, any> | null = null;
+  scopedMutation: ((...args: any[]) => any) | null = null;
 
-  override onBeforeUpdate(currentState: State) {
-    this.previousState = currentState;
+  override onBeforeUpdate(state: State) {
+    this.previousState = Object.freeze({ ...state.currentState });
   }
 
-  override onUpdate(nextState: State) {
-    console.log("Mutation:", nextState.name, "@", this.time);
-    console.log("      Previous State:", this.previousState);
-    console.log("      Next State    :", nextState);
+  override onUpdate(state: State) {
+    console.log(
+      "%cMutation:",
+      "color: rgb(187, 186, 186); font-weight: bold",
+      state.name,
+      "@",
+      this.time
+    );
+    console.log(
+      "   %cPrevious State",
+      "color: #26ad65; font-weight: bold",
+      this.previousState
+    );
+    console.log(
+      "   %cNext State    ",
+      "color: rgb(17, 118, 249); font-weight: bold",
+      state.getState()
+    );
     this.previousState = null;
   }
 
@@ -19,8 +34,10 @@ export class Logger extends Middleware {
     const date = new Date();
     const mHours = date.getHours();
     const hours = mHours > 12 ? mHours - 12 : mHours;
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const mins = date.getMinutes();
+    const minutes = mins.toString().length === 1 ? `0${mins}` : mins;
+    const secs = date.getSeconds();
+    const seconds = secs.toString().length === 1 ? `0${secs}` : secs;
     const milliseconds = date.getMilliseconds();
     return `${hours}:${minutes}:${seconds}:${milliseconds}`;
   }
