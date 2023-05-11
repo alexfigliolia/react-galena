8d2701b2f67d78403c3bb403e47a9287220b1e40
Alex Figliolia <alexfigliolia@gmail.com>
2023-05-10 21:49:45 -0700

    Repair bottle-neck

--- src/Galena/State.ts
+++ src/Galena/State.ts
@@ -23,13 +23,35 @@ export class State<T extends any = any> extends Reactivity<T> {
     return this.initialState[key];
   }
 
-  public update = this.mutation((func: (state: T) => void) => {
-    func(this.currentState);
+  public update = this.mutation((func: (state: T) => void | Promise<void>) => {
+    return func(this.currentState);
   });
 
   public reset = this.mutation(() => {
     this.currentState = State.clone(this.initialState);
   });
+
+  public mutation<F extends (...args: any[]) => any>(func: F) {
+    return (...args: Parameters<F>): void => {
+      this.onBeforeUpdate(this);
+      const returnValue = func(...args);
+      if (returnValue instanceof Promise) {
+        void returnValue.then(() => {
+          void this.emitUpdate();
+          this.onUpdate(this);
+        });
+      } else {
+        void this.emitUpdate();
+        this.onUpdate(this);
+      }
+    };
+  }
+
+  private emitUpdate() {
+    return Promise.resolve(() => {
+      this.emitter.emit(this.name, this);
+    });
+  }
 
   public subscribe(callback: (nextState: State<T>) => void) {
     return this.emitter.on(this.name, callback);
@@ -37,16 +59,6 @@ export class State<T extends any = any> extends Reactivity<T> {
 
   public unsubscribe(ID: string) {
     return this.emitter.off(this.name, ID);
-  }
-
-  public mutation<F extends (...args: any[]) => any>(func: F) {
-    return async (...args: Parameters<F>): Promise<ReturnType<F>> => {
-      this.onBeforeUpdate(this);
-      const returnValue = func(...args);
-      await Promise.resolve(this.emitter.emit(this.name, this));
-      this.onUpdate(this);
-      return returnValue;
-    };
   }
 
   private static clone<T>(state: T): T {
