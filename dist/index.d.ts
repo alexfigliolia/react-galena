b2bd878b057b58a59e528f8ad3f74a3ea68d1053
Alex Figliolia <alexfigliolia@gmail.com>
2023-05-12 13:52:24 -0700

    Nuke dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { connectGalena } from "./connectGalena";
-export { connectState } from "./connectState";
-export { createUseGalena } from "./createUseGalena";
-export { createUseState } from "./createUseState";
